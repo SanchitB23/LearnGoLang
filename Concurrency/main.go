5061efca836cdd1b69c70bf299dd6d76d05dce55
sanchitb23 <dev.sanchitb23@gmail.com>
2025-02-06 00:05:28 +0530

    Use go Channels to Implement Concurrency

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -5,18 +5,27 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, done chan bool) {
 	fmt.Println("Hello!", phrase)
+	done <- true
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, done chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	done <- true
+	close(done)
 }
 
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
-	greet("I hope you're liking the course!")
+	done := make(chan bool)
+
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
+	go slowGreet("How ... are ... you ...?", done)
+	go greet("I hope you're liking the course!", done)
+
+	for range done {
+	}
+
 }
